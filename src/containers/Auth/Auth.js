8c4c06e0c3d6b167067fd1c0753054fa20ae9c1d
Karmov Arthur <Artur69Karmov@gmail.com>
2018-10-15 18:17:22 +0300

    add router

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Auth extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Auth</h1>
+      </div>
+    );
+  }
+}
