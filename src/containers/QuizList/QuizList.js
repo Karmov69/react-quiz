1d31b429173134a298fdc0470e78713ea71b3fae
Karmov Arthur <Artur69Karmov@gmail.com>
2018-10-16 11:46:42 +0300

    конец работы с роутингом

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
-
-class QuizList extends Component {
+import classes from "./QuizList.css";
+import { NavLink } from "react-router-dom";
+export default class QuizList extends Component {
   state = {};
+
+  renderQuizes() {
+    return [1, 2, 3].map((quiz, index) => {
+      return (
+        <li key={index}>
+          <NavLink to={"/quiz/" + quiz}>Тест {quiz}</NavLink>
+        </li>
+      );
+    });
+  }
   render() {
     return (
-      <div>
-        <h1>Quiz List</h1>
+      <div className={classes.QuizList}>
+        <div>
+          <h1>Список тестов</h1>
+          <ul>{this.renderQuizes()}</ul>
+        </div>
       </div>
     );
   }
 }
-
-export default QuizList;
